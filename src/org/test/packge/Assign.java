63551ee36ccaca090e3e8fe4c0029db0879ff6bf
sivamanisridhar <sivamanisridhar07@gmal.com>
2019-04-02 20:33:03 +0530

    sridhar task added

---
+++ src/org/test/packge/Assign.java
@@ -0,0 +1,16 @@
+package org.test.packge;
+
+public class Assign {
+	public static void main(String[] args) {
+		String str = "sridhar is cheif minister";
+		int count = 0;
+		for (int i=0;i<str.length();i++ ){
+		char CharAt = str.charAt(i);
+		if (CharAt=='a');
+		count++;
+		System.out.println(count);
+		}
+			
+	}
+
+}
